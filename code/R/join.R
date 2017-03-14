1b22499201907d093373140d4a80cbfa2066f3f1
Akhil Nair <akhilm_nair@outlook.com>
2017-03-14 17:06:18 -0400

    add data join code

--- code/R/join.R
+++ code/R/join.R
@@ -1,3 +1,6 @@
 
-
 ## merge the datasets
+train_bodies <- read.csv("train_bodies.csv",header = TRUE,sep = ",",encoding = "UTF-8")
+train_stances <- read.csv("train_stances.csv",header = TRUE,sep = ",",encoding = "UTF-8")
+train<- merge(train_bodies,train_stances,by="Body.ID")
+write.csv(train,file = "train.csv",row.names = FALSE)
