0c3906d8d6013415b15e620d927d2228188f972a
Ankit <phaterepekar.ankit@gmail.com>
2017-04-10 16:43:36 -0400

    visualization check-in

--- code/python/classifier.py
+++ code/python/classifier.py
@@ -24,46 +24,46 @@ def train_XGB(data, test):
     Arguments:
     - `data`:
     """
-    predictors = ["overlapping","reoccur1", "reoccur2", "reoccur3","reoccur4", "reoccur5", "reoccur6","euclidean"]#,"cosine"]#,"wmdistance", "euclidean"]
+    predictors = ["overlapping","reoccur1", "reoccur2", "reoccur3","reoccur4", "reoccur5", "reoccur6","euclidean","refuting_feature_count","char_length_headline","char_length_body"]#,"cosine"]#,"wmdistance", "euclidean"]
     response = data.Stance
 
 
-    n_estimators = range(50,500, 50)
-    depth = range(1,10)
-
-    param_grid = dict(n_estimators=n_estimators, max_depth = depth)
-
-    kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=7)
-
-    model = xgb.XGBClassifier()
-
-    grid_search = GridSearchCV(model, param_grid, scoring="neg_log_loss", n_jobs=-1, cv=kfold)
-    grid_result = grid_search.fit(data[predictors], response)
-
-    print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
-
-
-
-    # gbm = xgb.XGBClassifier(max_depth=depth, n_estimators=300, learning_rate=0.05).fit(data[predictors], response)
-
-    # _test = test[["overlapping", "reoccur1", "reoccur2", "reoccur3","reoccur4", "reoccur5", "reoccur6","euclidean"]]#,"cosine"#,"wmdistance", "euclidean"]]
-    # _predictions = gbm.predict(_test)
-
-    # predictions = pd.Series(_predictions.tolist())
-    # test["predicted_XGB"] = predictions.values
+    # n_estimators = range(50,500, 50)
+    # depth = range(1,10)
+    #
+    # param_grid = dict(n_estimators=n_estimators, max_depth = depth)
+    #
+    # kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=7)
+    #
+    # model = xgb.XGBClassifier()
+    #
+    # grid_search = GridSearchCV(model, param_grid, scoring="neg_log_loss", n_jobs=-1, cv=kfold)
+    # grid_result = grid_search.fit(data[predictors], response)
+    #
+    # print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
 
 
-    # ## Accuracy calculation
-    # test["is_correct_prediction_XGB"] = test["Stance"] == test["predicted_XGB"]
-    # correctly_predicted_rows = test[test['is_correct_prediction_XGB'] == True]
 
-    # print("Accuracy : ", float(len(correctly_predicted_rows))/len(test))
+    gbm = xgb.XGBClassifier(max_depth=3, n_estimators=300, learning_rate=0.05).fit(data[predictors], response)
+
+    _test = test[["overlapping", "reoccur1", "reoccur2", "reoccur3","reoccur4", "reoccur5", "reoccur6","euclidean","refuting_feature_count","char_length_headline","char_length_body"]]#,"cosine"#,"wmdistance", "euclidean"]]
+    _predictions = gbm.predict(_test)
+
+    predictions = pd.Series(_predictions.tolist())
+    test["predicted_XGB"] = predictions.values
+
+
+    ## Accuracy calculation
+    test["is_correct_prediction_XGB"] = test["Stance"] == test["predicted_XGB"]
+    correctly_predicted_rows = test[test['is_correct_prediction_XGB'] == True]
+
+    print("Accuracy : ", float(len(correctly_predicted_rows))/len(test))
 
 
     
 def train_SVM(data):
 
-     predictors = ["overlapping","reoccur1", "reoccur2", "reoccur3", "reoccur4", "reoccur5", "reoccur6","euclidean"]
+     predictors = ["overlapping","reoccur1", "reoccur2", "reoccur3", "reoccur4", "reoccur5", "reoccur6","euclidean","refuting_feature_count","char_length_headline","char_length_body"]
      response = data.Stance
 
      clf = sklearn.svm.LinearSVC().fit(data[predictors],response)
@@ -78,7 +78,7 @@ def train_logistic(data):
     Arguments:
     - `data`:
     """
-    predictors = ["overlapping","reoccur1", "reoccur2", "reoccur3", "reoccur4", "reoccur5", "reoccur6","euclidean"]
+    predictors = ["overlapping","reoccur1", "reoccur2", "reoccur3", "reoccur4", "reoccur5", "reoccur6","euclidean""refuting_feature_count","char_length_headline","char_length_body"]
     response = data.Stance
 
     logreg = linear_model.LogisticRegression(C=1e5)
@@ -97,10 +97,10 @@ def randomForest (train,test):
     - `data`:
     """
 
-    predictors = ["overlapping","reoccur1", "reoccur2", "reoccur3","reoccur4", "reoccur5", "reoccur6", "euclidean"]#,"cosine"]#,"wmdistance", "euclidean"]
+    predictors = ["overlapping","reoccur1", "reoccur2", "reoccur3","reoccur4", "reoccur5", "euclidean","refuting_feature_count","char_length_headline","char_length_body"]#,"cosine"]#,"wmdistance", "euclidean"]
     response = train.Stance
 
-    _test = test[["overlapping", "reoccur1", "reoccur2", "reoccur3","reoccur4", "reoccur5", "reoccur6","euclidean"]]#,"cosine"]] #,"wmdistance", "euclidean"]]
+    _test = test[["overlapping", "reoccur1", "reoccur2", "reoccur3","reoccur4", "reoccur5","euclidean","refuting_feature_count","char_length_headline","char_length_body"]]#,"cosine"]] #,"wmdistance", "euclidean"]]
 
     clf = RandomForestClassifier(n_jobs=2)
     clf.fit(train[predictors],response)
